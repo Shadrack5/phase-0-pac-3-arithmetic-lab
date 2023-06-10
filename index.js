d4c43272ffaeac217f508da470bdb877a366f106
Shadrack5 <shadrackkibet100@gmail.com>
2023-06-10 18:30:19 +0300

    completed assingment

---
+++ index.js
@@ -0,0 +1,51 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a,b){
+  }
+
+  function subtract(a, b) {
+    return a - b;
+}
+function multiply(a,b){
+  }
+
+  function multiply(a, b) {
+    return a * b;
+}
+function divide(a,b){
+  }
+
+  function divide(a, b) {
+    return a / b;
+}
+function decrement(n){
+  }
+
+  function decrement(n) {
+    return n - 1;
+}
+function increment(n){
+  }
+
+  function increment(n) {
+    return n + 1;
+}
+function makeInt(n){
+  }
+
+  function makeInt(n) {
+    return parseInt(n, 10);
+}
+function makeInt(n){
+    return NaN
+}
+function preserveDecimal(n){
+  }
+
+  function preserveDecimal(n) {
+    return parseFloat(n);
+}
+function preserveDecimal(n){
+    return NaN;
+}
